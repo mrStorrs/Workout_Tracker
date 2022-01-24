ebbedad3d652761754c5882457d9b7399b052dfc
mrStorrs <cj.storrs@gmail.com>
2022-01-24 12:29:11 -0800

    Added Title over users button. Fixed main parameter call.

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-    public static void main(String args[]){
-        JFrame frame = new JFrame("My First GUI");
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(200,100);
 
-        JPanel panel = new JPanel();
+        //creating user heading panel
+        JPanel user_heading = new JPanel();
+        user_heading.add(new JLabel("Current Users"));
+
+        //creating user buttons panel, and buttons.
+        JPanel user_buttons_panel = new JPanel();
         JButton button1 = new JButton("Button 1");
         JButton button2 = new JButton("Button 2");
-        panel.add(button1);
-        panel.add(button2);
-        frame.getContentPane().add(panel);
+        user_buttons_panel.add(button1);
+        user_buttons_panel.add(button2);
 
+        //adding panels
+        frame.getContentPane().add(BorderLayout.NORTH, user_heading);
+        frame.getContentPane().add(BorderLayout.SOUTH, user_buttons_panel);
         frame.setVisible(true);
     }
 }
