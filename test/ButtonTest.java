5ce1b7749e3850ca6670a76e981e05b1d5893986
mrStorrs <cj.storrs@gmail.com>
2022-01-25 10:52:05 -0800

    Added tests for Button class.

---
+++ test/ButtonTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ButtonTest {
+    static Button a, b;
+
+
+
+    @BeforeAll
+    public static void setUp(){
+        a = new Button("button-a");
+        b = new Button("button-b");
+
+    }
+
+    @Test
+    void actionPerformed() {
+    }
+
+    @Test
+    void setLabel_text() {
+        a.setLabel_text("button-a-changed");
+        assertEquals("button-a-changed", a.getLabel_text());
+    }
+
+    @Test
+    void getLabel_text() {
+        assertEquals("button-a", a.getLabel_text());
+    }
+}
