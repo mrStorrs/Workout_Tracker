21c1c3eaa663641fca8b09adfeafec2474093344
mrStorrs <cj.storrs@gmail.com>
2022-01-24 16:08:07 -0800

    Changed adding users to be added to an ArrayList to be iterated through. Added dynamic adding of buttons from said list.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Main {
+
+    private static Boolean DEBUGGING = true;
+    private static Debug main_debug = new Debug("Main");
+
     public static void main(String[] args){
+        //creating a user list.
+        //this will eventually be moved to being done via gui.
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("cj"));
+        users.add(new User("bob"));
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,12 +25,16 @@ public class Main {
 
         //creating user buttons panel, and buttons.
         JPanel user_buttons_panel = new JPanel();
-        JButton button1 = new JButton("Button 1");
-        JButton button2 = new JButton("Button 2");
-        user_buttons_panel.add(button1);
-        user_buttons_panel.add(button2);
 
-
+        //dynamically creating user buttons.
+        for(User user : users){
+            JButton button = new JButton(user.getName());
+            user_buttons_panel.add(button);
+            if(DEBUGGING){
+                main_debug.start_debug();
+                System.out.println("Adding Button for user: " + user.getName());
+            }
+        }
 
         //adding panels
         frame.getContentPane().add(BorderLayout.NORTH, user_heading);
